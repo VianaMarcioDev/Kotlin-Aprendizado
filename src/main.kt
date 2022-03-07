936abd4d5c51a21b23f1d066035f6bc79710fb2b
Márcio de Freitas Viana <83033976+VianaMarcioDev@users.noreply.github.com>
2022-03-06 23:23:54 -0300

    Criar arquivo testaCondicoes para organizar o projeto

--- src/main.kt
+++ src/main.kt
@@ -84,18 +84,3 @@ class Conta(
 
 
 
-fun testaCondicoes(saldo: Double) {
-    if (saldo > 0.0) {
-        println("conta é positiva")
-    } else if (saldo == 0.0) {
-        println("conta é neutra")
-    } else {
-        println("conta é negativa")
-    }
-
-    when {
-        saldo > 0.0 -> println("conta é positiva")
-        saldo == 0.0 -> println("conta é neutra")
-        else -> println("conta é negativa")
-    }
-}
