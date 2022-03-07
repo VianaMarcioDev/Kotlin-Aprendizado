80465df899ea577e4b079e4be65c1bd3e71119c2
Márcio de Freitas Viana <83033976+VianaMarcioDev@users.noreply.github.com>
2022-03-06 23:22:45 -0300

    Criar arquivo testaLacos para organizar o projeto

--- src/main.kt
+++ src/main.kt
@@ -82,31 +82,7 @@ class Conta(
 
 
 
-fun testaLacos() {
-    var i = 0
-    while (i < 5) {
-        val titular: String = "Alex $i"
-        val numeroConta: Int = 1000 + i
-        var saldo = i + 10.0
 
-        println("titular $titular")
-        println("número da conta $numeroConta")
-        println("saldo da conta $saldo")
-        println()
-        i++
-    }
-
-    for (i in 5 downTo 1) {
-        val titular: String = "Alex $i"
-        val numeroConta: Int = 1000 + i
-        var saldo = i + 10.0
-
-        println("titular $titular")
-        println("número da conta $numeroConta")
-        println("saldo da conta $saldo")
-        println()
-    }
-}
 
 fun testaCondicoes(saldo: Double) {
     if (saldo > 0.0) {
