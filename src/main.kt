5c467fde05210337154a285b88fc5b01a368386a
Márcio de Freitas Viana <83033976+VianaMarcioDev@users.noreply.github.com>
2022-03-06 22:39:04 -0300

    Primeiro commit. Criar arquivo testaCopiasEReferencias para organizar código

---
+++ src/main.kt
@@ -0,0 +1,125 @@
+fun main() {
+    println("Bem vindo ao Bytebank")
+
+    val contaAlex = Conta(titular = "Alex", numero = 1000)
+    contaAlex.deposita(200.0)
+
+    val contaFran = Conta(numero = 1001, titular = "Fran")
+    contaFran.deposita(300.0)
+
+    println(contaFran.titular)
+    println(contaFran.numero)
+    println(contaFran.saldo)
+
+    println(contaAlex.titular)
+    println(contaAlex.numero)
+    println(contaAlex.saldo)
+
+    println("depositando na conta do Alex")
+    contaAlex.deposita(50.0)
+    println(contaAlex.saldo)
+
+    println("depositando na conta da Fran")
+    contaFran.deposita(70.0)
+    println(contaFran.saldo)
+
+    println("sacando na conta do Alex")
+    contaAlex.saca(250.0)
+    println(contaAlex.saldo)
+
+    println("sacando na conta da Fran")
+    contaFran.saca(100.0)
+    println(contaFran.saldo)
+
+    println("saque em excesso na conta do Alex")
+    contaAlex.saca(100.0)
+    println(contaAlex.saldo)
+
+    println("saque em excesso na conta da Fran")
+    contaFran.saca(500.0)
+    println(contaFran.saldo)
+
+    println("Transferência da conta da Fran para o Alex")
+
+    if (contaFran.transfere(destino = contaAlex, valor = 300.0)) {
+        println("Transferência sucedida")
+    } else {
+        println("Falha na transferência")
+    }
+
+    println(contaAlex.saldo)
+    println(contaFran.saldo)
+}
+
+class Conta(
+    var titular: String,
+    val numero: Int
+) {
+    var saldo = 0.0
+        private set
+
+    fun deposita(valor: Double) {
+        if (valor > 0) {
+            this.saldo += valor
+        }
+    }
+
+    fun saca(valor: Double) {
+        if (saldo >= valor) {
+            saldo -= valor
+        }
+    }
+
+    fun transfere(valor: Double, destino: Conta): Boolean {
+        if (saldo >= valor) {
+            saldo -= valor
+            destino.deposita(valor)
+            return true
+        }
+        return false
+    }
+}
+
+
+
+fun testaLacos() {
+    var i = 0
+    while (i < 5) {
+        val titular: String = "Alex $i"
+        val numeroConta: Int = 1000 + i
+        var saldo = i + 10.0
+
+        println("titular $titular")
+        println("número da conta $numeroConta")
+        println("saldo da conta $saldo")
+        println()
+        i++
+    }
+
+    for (i in 5 downTo 1) {
+        val titular: String = "Alex $i"
+        val numeroConta: Int = 1000 + i
+        var saldo = i + 10.0
+
+        println("titular $titular")
+        println("número da conta $numeroConta")
+        println("saldo da conta $saldo")
+        println()
+    }
+}
+
+fun testaCondicoes(saldo: Double) {
+    if (saldo > 0.0) {
+        println("conta é positiva")
+    } else if (saldo == 0.0) {
+        println("conta é neutra")
+    } else {
+        println("conta é negativa")
+    }
+
+    when {
+        saldo > 0.0 -> println("conta é positiva")
+        saldo == 0.0 -> println("conta é neutra")
+        else -> println("conta é negativa")
+    }
+}
